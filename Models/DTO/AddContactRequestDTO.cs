421f1f0472b2eb863be266a64d273622eb21dc0e
pavanmenugonda <pavanmenugonda9@gmail.com>
2024-10-30 19:05:53 +0930

    files added

---
+++ Models/DTO/AddContactRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Contactly.Models.DTO
+{
+    public class AddContactRequestDTO
+    {
+        public required string Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool Favorite { get; set; }
+    }
+}
